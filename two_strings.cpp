e6c6433d158e5d0e097eec25fdb26f9e01724842
Yusuf Turan <yusuftrn@rocketmail.com>
2020-09-25 00:21:16 +0300

    Create two_strings.cpp

---
+++ two_strings.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    int t; cin>>t;
+    while(t--){
+        string s1, s2;
+        cin>>s1>>s2;
+        sort(s1.begin(),s1.end());
+        sort(s2.begin(),s2.end());
+
+        if(s1==s2)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+}

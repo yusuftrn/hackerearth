57d672554dd6fb32fad1c4b4969aa3bcf281137c
Yusuf Turan <yusuftrn@rocketmail.com>
2020-09-26 01:38:58 +0300

    Update life_the-universe_and-everything.cpp

--- life_the-universe_and-everything.cpp
+++ life_the-universe_and-everything.cpp
@@ -1,12 +1,9 @@
 #include <bits/stdc++.h>
-#define ll long long
 using namespace std;
 int main(void){
-    ll x = 10000;
-    int y;
+    int y,x = 10000;
     int flag = 0;
     for(int i=0;i<x;i++){
-        int y;
         cin>>y;
         if(y!=42 && flag == 0) {
             cout<<y<<endl;
